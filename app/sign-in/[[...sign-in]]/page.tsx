1baa4c4467809583e32f95e495a5d31864ad9a58
mestresdatattoocontato-netizen <262137535+mestresdatattoocontato-netizen@users.noreply.github.com>
2026-08-07 14:18:58 -0300

    fix: remove outdated redirectUrl prop from clerk v7

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -3,10 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function SignInPage() {
   return (
     <div className="min-h-screen bg-black flex items-center justify-center pt-20">
-      <SignIn
-        redirectUrl="/dashboard"
-        signUpUrl="/sign-up"
-      />
+      <SignIn />
     </div>
   );
 }
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -3,10 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function SignUpPage() {
   return (
     <div className="min-h-screen bg-black flex items-center justify-center pt-20">
-      <SignUp
-        redirectUrl="/dashboard"
-        signInUrl="/sign-in"
-      />
+      <SignUp />
     </div>
   );
 }
